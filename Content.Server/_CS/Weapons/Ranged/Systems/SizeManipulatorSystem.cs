313a62abcb7ef07c334f720cf608742a93522f08
Shawn McHenry <drraccoony@gmail.com>
2025-12-06 01:32:24 -0600

    Size manipluation gun works with remote signaler

--- Content.Server/_CS/Weapons/Ranged/Systems/SizeManipulatorSystem.cs
+++ Content.Server/_CS/Weapons/Ranged/Systems/SizeManipulatorSystem.cs
@@ -1,14 +1,21 @@
 using Content.Server.Body.Systems;
+using Content.Server.DeviceLinking.Systems;
+using Content.Server.Weapons.Ranged.Components;
+using Content.Shared.DeviceLinking.Events;
 using Content.Shared.Projectiles;
 using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Events;
 using Robust.Shared.Log;
+using Robust.Shared.Map;
+using System.Numerics;
 
 namespace Content.Server.Weapons.Ranged.Systems;
 
 public sealed class SizeManipulatorSystem : EntitySystem
 {
     [Dependency] private readonly SizeManipulationSystem _sizeManipulation = default!;
+    [Dependency] private readonly DeviceLinkSystem _deviceLink = default!;
+    [Dependency] private readonly GunSystem _gunSystem = default!;
 
     public override void Initialize()
     {
@@ -16,6 +23,50 @@ public sealed class SizeManipulatorSystem : EntitySystem
 
         SubscribeLocalEvent<SizeManipulatorComponent, AmmoShotEvent>(OnAmmoShot);
         SubscribeLocalEvent<BulletSizeManipulatorComponent, ProjectileHitEvent>(OnProjectileHit);
+
+        SubscribeLocalEvent<FireOnSignalComponent, SignalReceivedEvent>(OnSignalReceived);
+        SubscribeLocalEvent<FireOnSignalComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnSignalReceived(EntityUid uid, FireOnSignalComponent component, ref SignalReceivedEvent args)
+    {
+        if (!TryComp<GunComponent>(uid, out var gun) || !TryComp<SizeManipulatorComponent>(uid, out var sizeManip))
+            return;
+
+        // Determine which mode to use based on which port received the signal
+        SizeManipulatorMode? modeToUse = null;
+
+        if (args.Port == component.GrowPort)
+            modeToUse = SizeManipulatorMode.Grow;
+        else if (args.Port == component.ShrinkPort)
+            modeToUse = SizeManipulatorMode.Shrink;
+
+        if (modeToUse == null)
+            return;
+
+        // Set the mode before firing
+        sizeManip.Mode = modeToUse.Value;
+        Dirty(uid, sizeManip);
+
+        // Update the projectile prototype based on the mode
+        if (TryComp<ProjectileBatteryAmmoProviderComponent>(uid, out var projectileProvider))
+        {
+            projectileProvider.Prototype = modeToUse == SizeManipulatorMode.Grow
+                ? sizeManip.GrowPrototype
+                : sizeManip.ShrinkPrototype;
+            Dirty(uid, projectileProvider);
+        }
+
+        // Fire the gun - rotate 90 degrees counter-clockwise from the gun's default direction
+        // Guns shoot down by default, so rotating 90 degrees makes them shoot in the visual direction
+        var dir = gun.DefaultDirection;
+        dir = new Vector2(-dir.Y, dir.X); // 90 degrees counter-clockwise rotation
+        _gunSystem.AttemptShoot(uid, uid, gun, new EntityCoordinates(uid, dir));
+    }
+
+    private void OnInit(EntityUid uid, FireOnSignalComponent component, ComponentInit args)
+    {
+        _deviceLink.EnsureSinkPorts(uid, component.GrowPort, component.ShrinkPort);
     }
 
     private void OnAmmoShot(EntityUid uid, SizeManipulatorComponent component, AmmoShotEvent args)
