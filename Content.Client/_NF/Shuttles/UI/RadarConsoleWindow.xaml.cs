8e10166c3efba92be8db76692afe1488b633adc4
Shawn McHenry <drraccoony@gmail.com>
2025-12-10 21:10:38 -0600

    Add IFF search to mass scanner

--- Content.Client/_NF/Shuttles/UI/RadarConsoleWindow.xaml.cs
+++ Content.Client/_NF/Shuttles/UI/RadarConsoleWindow.xaml.cs
@@ -1,12 +1,40 @@
 using Content.Client.Computer;
 using Content.Client.UserInterface.Controls;
 using Content.Shared.Shuttles.BUIStates;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.GameObjects;
+using System;
 
 namespace Content.Client.Shuttles.UI;
 
 public sealed partial class RadarConsoleWindow : FancyWindow,
     IComputerWindow<NavInterfaceState>
 {
+    [Dependency] private readonly IEntityManager _entManager = default!;
+
+    protected override void EnteredTree()
+    {
+        base.EnteredTree();
+
+        IoCManager.InjectDependencies(this);
+
+        // Wire up IFF search
+        IffSearchCriteria.OnTextChanged += OnIffSearchChanged;
+    }
+
+    private void OnIffSearchChanged(LineEdit.LineEditEventArgs args)
+    {
+        var text = args.Text.Trim();
+
+        RadarScreen.IFFFilter = text.Length == 0
+            ? null // If empty, do not filter
+            : (entity, grid, iff) => // Otherwise use simple search criteria
+            {
+                _entManager.TryGetComponent<MetaDataComponent>(entity, out var metadata);
+                return metadata != null && metadata.EntityName.Contains(text, StringComparison.OrdinalIgnoreCase);
+            };
+    }
+
     public void SetConsole(EntityUid consoleEntity)
     {
         RadarScreen.SetConsole(consoleEntity);
--- Resources/Locale/en-US/misc-computers.ftl
+++ Resources/Locale/en-US/misc-computers.ftl
@@ -1,2 +1,4 @@
 radar-console-window-title = Mass Scanner Console
+radar-console-iff-search = IFF Search
+radar-console-angle-labels = Angle Labels
 shuttle-console-window-title = Shuttle Console
